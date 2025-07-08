2a7f2edaf514c820b41295b110c9afd4f9fe3365
omsavant24 <omsawant1307@gmail.com>
2025-07-08 16:22:25 +0530

    Updated Welcome screen layout

---
+++ screens/WalletScreen.tsx
@@ -0,0 +1,39 @@
+// WalletScreen.tsx
+import React from 'react';
+
+
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import WalletHeader, { BarItem } from '../components/WalletHeader';
+
+const bars: BarItem[] = [
+  { color: '#33C48D', label: 'Netflix', width: 100, usage: 12, marginRight: 20, shiftX: -40 },
+  { color: '#6464FF', label: 'Prime', width: 100, usage: 8, marginRight: 20, shiftX: -30 },
+  { color: '#D965C7', label: 'Hotstar', width: 100, usage: 16, marginRight: 20, shiftX: -20 },
+  { color: '#33FF57', label: 'YouTube', width: 100, usage: 20, marginRight: 20, shiftX: -10 },
+  { color: '#33FFF6', label: 'Spotify', width: 100, usage: 10, marginRight: 20, shiftX: 0 },
+  { color: '#33FF99', label: 'HealthyFy', width: 100, usage: 9, marginRight: 20, shiftX: 10 },
+];
+
+export default function WalletScreen() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <WalletHeader
+          walletAddress="Subscribed-Apps"
+          userName="Om"
+          balanceAmount="₹7000"
+          bars={bars}
+        />
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+});
